9b93edbef63e2db3352abfcfb56dc42b4e4bdeef
J Neal <jasmineneal101@gmail.com>
2023-05-14 21:48:50 +1000

    Add models code

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+//do I need to add each route folder i.e. const categoryRoutes = require("./category-routes")
 
 router.use('/api', apiRoutes);
 
